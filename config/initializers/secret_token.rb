10d9208fb4a5f992a22647037270fa7e1d8a2c52
takanori-matsumoto-mulodo <takanori.matsumoto@mulodo.com>
2014-04-01 13:18:32 +0900

    initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TmTraining::Application.config.secret_key_base = '7bb4e6d14bb438a56e6ab959de0dd8fb278c685cc0ed232ce5f9d7f67199d2ba89eeb1bb9ebcf0d00023c5c303d535cb867f98985a5510f58e52bcc1ff1ae65f'
