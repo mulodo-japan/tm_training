8be6d4df99422999cdde708e578304bbc61dad37
takanori-matsumoto-mulodo <takanori.matsumoto@mulodo.com>
2014-04-03 16:39:25 +0900

    Add utility file including full_title funciton.
    Shorten test code for static_pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,52 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject { page }
+  
   describe "Home page" do
+    before { visit home_path }
     
-    it "should have the content 'Sample App'" do
-      visit home_path
-      expect(page).to have_content('Sample App')
-    end
-    
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-    
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home|')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home|') }
   end
   
   describe "Help page" do
+    before { visit help_path }
     
-    it "should have the 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-    
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('')) }
   end
   
   describe "About page" do
-
-    it "should have the 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
     
-    it "should have the content 'Contact" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About')) }
+  end
+  
+  describe "Contact page" do
+    before { visit contact_path }
     
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
