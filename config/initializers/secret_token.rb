bb2f10b815b14ee0a9ed5a98fa346bb9a94436c8
takanori-matsumoto-mulodo <takanori.matsumoto@mulodo.com>
2014-04-03 11:48:46 +0900

    Make rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitTest::Application.config.secret_key_base = '03e8feb4edd37e5401587163bfa4834a3d347026392adac66db6dbb34702bc6e18f77208dca4b212287ef336d073d545d146b1aee82fce3c5c05744d00e91d7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitTest::Application.config.session_store :cookie_store, key: '_git_test_session'
