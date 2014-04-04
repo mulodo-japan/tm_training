c508f6d59aab78e42eea1bc5e28a12a0d37f1b58
takanori-matsumoto-mulodo <takanori.matsumoto@mulodo.com>
2014-04-04 11:44:00 +0900

    Aad user model and test for it.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe User do
+  
+  before { @user = User.new(name: "Examle User", email: "user@example.com") }
+
+  subject { @user }
+  
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  
+  it { should be_valid }
+  
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+  
+  describe "when name is valid long" do
+    before { @user.name = "a" * 50 }
+    it { should be_valid }
+  end
+  
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[aaa@aa,aa fdasdf.asdf.fff exapmle.user@foo.foo@aaa.com fff@aa++a]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+  
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[takanori.matsumoto@aaa.com aaa@aa.aa test@test.test]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+end
